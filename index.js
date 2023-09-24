981ee489f14ee01a1a5819bbc121e26e20f80953
MSIgamer <ramnoah2308@gmail.com>
2023-09-24 16:03:55 -0600

    activity 8

--- index.js
+++ index.js
@@ -7,17 +7,15 @@ const morgan = require ('morgan');
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-//SE USA P
-
 
 app.get("/", (req, res, next) => {
-    return res.status(200).send("Welcome to Pokedex");
+    return res.status(200).json({code: 1, message:"Welcome to pokedex"});
 }); 
 //REINICIAR SERVIDOR DE FORMA AUTOMATICA
 app.use("/pokemon",pokemon);
 
 app.use((req, res, next) =>{
-    return res.status(404).json({code: 404, message: "URL NO ENCONTRADA :("});
+    return res.status(404).json({code: 404, message: "URL NO ENCONTRADA "});
 });
 
 app.listen(process.env.PORT || 3000,() =>{
