8ce0d38ad1f1fb2c7ee2a6e502edc8a8c00f02a8
MSIgamer <ramnoah2308@gmail.com>
2023-10-01 14:43:01 -0600

    activity 11

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 const express=require('express');
 const app = express();
-const pokemon = require('./routes/pokemon')
+const pokemon = require('./routes/pokemon');
 const morgan = require ('morgan');
+const user = require('./routes/user');
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -11,8 +12,9 @@ app.use(express.urlencoded({extended: true}));
 app.get("/", (req, res, next) => {
     return res.status(200).json({code: 1, message:"Welcome to pokedex"});
 }); 
-//REINICIAR SERVIDOR DE FORMA AUTOMATICA
+
 app.use("/pokemon",pokemon);
+app.use("/user",user);
 
 app.use((req, res, next) =>{
     return res.status(404).json({code: 404, message: "URL NO ENCONTRADA "});
